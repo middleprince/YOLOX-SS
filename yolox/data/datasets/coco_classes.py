d2f3c967ab1b637c3135f5c24ffef347689ddc50
younghu_i <younghu_i@didiglobal.com>
2021-09-25 04:31:59 +0800

    faet: Add dataset

---
+++ yolox/data/datasets/coco_classes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Copyright (c) Megvii, Inc. and its affiliates.
+
+#COCO_CLASSES = (
+#   "person",
+#   "carro",
+#   "car",
+#   )
+#COCO_CLASSES = (
+#    "Person"
+#    )
+
+COCO_CLASSES = (
+    "yarn",
+    "yarn-break"
+    )
